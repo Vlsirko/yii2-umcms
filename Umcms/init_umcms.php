fc10147ee8a8d920dbf7cc238669b738e864843a
Vlsirko <Vlsirko@i.ua>
2015-12-02 09:40:05 +0200

    init

--- Umcms/init_umcms.php
+++ Umcms/init_umcms.php
@@ -6,13 +6,14 @@
 /**
  * create upload dir
  */
-$pathToBackend = __DIR__ . '/../../../backend';
-$pathToFrontend = __DIR__ . '/../../../frontend';
+$pathToBackend = __DIR__ . '/../../../../backend';
+$pathToFrontend = __DIR__ . '/../../../../frontend';
 $pathToUploadBackend = $pathToBackend . '/web/upload';
 $pathToUploadFrontend = $pathToFrontend . '/web/upload';
+$pathToRoot = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..','..', '..',]);
 
 if (!is_dir($pathToUploadBackend)) {
-	mkdir($pathToUploadBackend, 0777);
+	mkdir($pathToUploadBackend, 0777, true);
 	print('Upload dir is created' . PHP_EOL);
 }
 
@@ -22,5 +23,43 @@ if (!is_dir($pathToUploadBackend)) {
 if (!is_link($pathToUploadFrontend) && symlink($pathToUploadBackend, $pathToUploadFrontend)) {
 	print('Symlink for upload dir to frontend is created' . PHP_EOL);
 }
+
+/**
+ * rewrite backend config
+ */
+print('Rewrite config file' . PHP_EOL);
+$pathToBackendConfig = $pathToBackend . '/config/main.php';
+
+$toRewrite = [
+	'basePath' => '\'' . __DIR__ . '\'',
+	'modules' =>   'require ("' . implode(DIRECTORY_SEPARATOR, [__DIR__, 'config', 'modules.php']) . '")',
+	'identityClass' => "'Umcms\models\User'"
+];
+$config = file_get_contents($pathToBackendConfig);
+
+foreach($toRewrite as $key => $value){
+	$config =  preg_replace("#'{$key}' => ([^,]+)#mi", "'{$key}' => $value", $config);
+}
+
+file_put_contents($pathToBackendConfig, $config);
+print('Rewrite config file success' . PHP_EOL);
+
+print('Run migrations' . PHP_EOL);
+
+$migrationPathes = [
+	'',
+	'@yii/rbac/migrations',
+	__DIR__ . '/migrations/',
+	__DIR__ . '/../../yii2-rbac/migrations',
+	__DIR__ . '/../../yii2-uploads/Uploads/migrations'
+];
+
+$command = $pathToRoot . '/yii migrate';
+foreach($migrationPathes as $path){
+	$commandPath = $path ? '--migrationPath=' . $path : '';
+	print("enter 'yes' to apply migration' {$path}" . PHP_EOL);
+	print(shell_exec($command . ' ' . $commandPath));
+}
+
 
 exit;
